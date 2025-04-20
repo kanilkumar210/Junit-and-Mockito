b091cf9d0228e9ad729a2e7e15e7a8e8303094fa
Venkatesh Kanike <anilkarre8@gmail.com>
2025-04-20 12:32:41 +0530

    fourth commit

--- src/main/java/com/company/employeeapplication/EmployeeApplication.java
+++ src/main/java/com/company/employeeapplication/EmployeeApplication.java
@@ -2,19 +2,18 @@ package com.company.employeeapplication;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EmployeeApplication {
+public class EmployeeApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EmployeeApplication.class, args);
 	}
 
-	//
-
-	///
-	////
-	///
-	///
-
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(EmployeeApplication.class);
+	}
 }
