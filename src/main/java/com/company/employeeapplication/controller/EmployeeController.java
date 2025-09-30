74382752bdd71680a183c147c36857c9c5034229
kanilkumar210 <anilkarre8@gmail.com>
2025-09-30 21:02:03 +0530

    commit all

--- src/main/java/com/company/employeeapplication/controller/EmployeeController.java
+++ src/main/java/com/company/employeeapplication/controller/EmployeeController.java
@@ -2,13 +2,11 @@ package com.company.employeeapplication.controller;
 
 import java.util.List;
 
+import com.company.employeeapplication.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.data.repository.query.Param;
+import org.springframework.web.bind.annotation.*;
 
 import com.company.employeeapplication.entity.Employee;
 import com.company.employeeapplication.service.IEmployeeService;
@@ -16,30 +14,34 @@ import com.company.employeeapplication.service.IEmployeeService;
 @RestController
 public class EmployeeController {
 
-	@Autowired
-	private IEmployeeService employeeService;
+    @Autowired
+    private IEmployeeService employeeService;
+//
+//    @PostMapping("/save")
+//    public Employee saveEmployee(@RequestBody Employee employee) {
+//        System.out.println("Controller...");
+//        return employeeService.saveEmployee(employee);
+//    }
 
-	@PostMapping("/save")
-	public Employee saveEmployee(@RequestBody Employee employee) {
-		System.out.println("Controller...");
-		return employeeService.saveEmployee(employee);
-	}
+    @GetMapping("/getEmployees")
+    public List<Employee> getAllEmployees() {
+        System.out.println("Controller...");
+        return employeeService.getAllEmployees();
+    }
 
-//	@GetMapping("/getEmployees")
-//	public List<Employee> getAllEmployees() {
-//		System.out.println("Controller...");
-//		return employeeService.getAllEmployees();
-//	}
+    @GetMapping("/getEmployeeById/{id}")
+    public Employee getEmployeeById(@PathVariable("id") long id) {
+        System.out.println("Controller...");
+        return employeeService.getEmployeeById(id);
+    }
 
-	@GetMapping("/getEmployeeById/{id}")
-	public Employee getEmployeeById(@PathVariable("id") long id) {
-		System.out.println("Controller...");
-		return employeeService.getEmployeeById(id);
-	}
+    @DeleteMapping("/deleteEmployee/{id}")
+    public String deleteEmployeeById(@PathVariable("id") long id) {
+        return employeeService.deleteEmployee(id);
+    }
 
-	@DeleteMapping("/deleteEmployee/{id}")
-	public String deleteEmployeeById(@PathVariable("id") long id) {
-		return employeeService.deleteEmployee(id);
-	}
-
+    @GetMapping("/getNamesBySalary")
+    public List<String> getNamesBySalary(@RequestParam("salary") long salary) {
+        return employeeService.getNamesBySalary(salary);
+    }
 }
